65365bdeeb7cfc99723f304644bf908ff132849a
Umesh Choudhary <choudharyumesh986@gmail.com>
2026-08-14 12:37:42 +0530

    Update: API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "http://localhost:7933/comments";
+const API = "https://nested-comment-system-1-mspx.onrender.com";
 
 function App() {
   const [comments, setComments] = useState([]);
